9ddb4e0fedef95105fec42d53dd88bf5933fd434
leejikgirl <103641561+leejikgirl@users.noreply.github.com>
2022-06-01 19:18:55 +0900

    Add files via upload

---
+++ moreapp/moreapp.php
@@ -0,0 +1,39 @@
+[
+
+{
+	"name": "갬성갤러리 - 인스타그램, 핀터레스트 감성 배경화면",
+	"eng" : "갬성갤러리 - 인스타그램, 핀터레스트 감성 배경화면",
+        "category": "Best",
+	"rating": 5.0,
+	"package_name": "com.leejikgirl.gamsunggallery",
+	"image": "https://play-lh.googleusercontent.com/T1qmve19AP5iCHv8m3awzT_8e-daEoGhCz03-z5SNsTjEjZGE2Re_wM4p7fL7PqaX-A=w480-h960-rw"
+},
+
+{
+	"name": "갬성갤러리 - 인스타그램, 핀터레스트 감성 배경화면",
+	"eng" : "갬성갤러리 - 인스타그램, 핀터레스트 감성 배경화면",
+        "category": "Wallpaper",
+	"rating": 5.0,
+	"package_name": "com.leejikgirl.gamsunggallery",
+	"image": "https://play-lh.googleusercontent.com/T1qmve19AP5iCHv8m3awzT_8e-daEoGhCz03-z5SNsTjEjZGE2Re_wM4p7fL7PqaX-A=w480-h960-rw"
+},
+
+{
+	"name": "동물힐링짤, 인생명언짤, MBTI 등 즐거운 짤방앱",
+	"eng" : "동물힐링짤, 인생명언짤, MBTI 등 즐거운 짤방앱",
+        "category": "Best",
+	"rating": 5.0,
+	"package_name": "com.leejikgirl.sunameme",
+	"image": "https://play-lh.googleusercontent.com/PvZTpwrwgrgGj3Gcrld3ZKJHgqF1rpY_EY0jQF9xR7a2mHpSMSWUEvRN71TdkO__n8w=w480-h960-rw"
+},
+
+{
+	"name": "동물힐링짤, 인생명언짤, MBTI 등 즐거운 짤방앱",
+	"eng" : "동물힐링짤, 인생명언짤, MBTI 등 즐거운 짤방앱",
+        "category": "Meme",
+	"rating": 5.0,
+	"package_name": "com.leejikgirl.sunameme",
+	"image": "https://play-lh.googleusercontent.com/PvZTpwrwgrgGj3Gcrld3ZKJHgqF1rpY_EY0jQF9xR7a2mHpSMSWUEvRN71TdkO__n8w=w480-h960-rw"
+}
+
+]
